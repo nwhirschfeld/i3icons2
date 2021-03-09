1612d7a9cb99ae2bdaa7c25a4145bbb21f5e3904
Ivan Milov <522518+ivanmilov@users.noreply.github.com>
2021-03-09 16:20:31 +0100

    Change ipc library The new one has tree helpers(Workspaces and Leaves) which make code easier

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ericm/i3icons2
 
 go 1.12
 
-require github.com/nwhirschfeld/i3ipc v0.0.0-20181125224120-4e621542b48c
+require github.com/mdirkse/i3ipc v0.0.0-20171212230543-ac599a872375
--- i3icons.go
+++ i3icons.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -10,17 +9,17 @@ import (
 	"os/user"
 	"strings"
 
-	"github.com/nwhirschfeld/i3ipc"
+	"github.com/mdirkse/i3ipc"
 )
 
 func main() {
 	usr, err := user.Current()
-    if err != nil {
-        log.Fatal( err )
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// handle command line arguments
-	var configFileName = flag.String("c", usr.HomeDir + "/.config/i3icons/i3icons2.config", "config file")
+	var configFileName = flag.String("c", usr.HomeDir+"/.config/i3icons/i3icons2.config", "config file")
 	var verbose = flag.Bool("v", false, "verbose")
 	flag.Parse()
 
@@ -46,101 +45,52 @@ func main() {
 
 	// open I3IPC socket and subscribe to window events
 	ipcsocket, _ := i3ipc.GetIPCSocket()
+	i3ipc.StartEventListener()
 	channel, err := i3ipc.Subscribe(i3ipc.I3WindowEvent)
 	EventLoop(channel, ipcsocket, config, *verbose)
-}
 
-// SubNodeByName gets the subnode of an I3Node by name
-func SubNodeByName(node *i3ipc.I3Node, name string) (root i3ipc.I3Node, err error) {
-	if strings.Compare(node.Name, name) == 0 {
-		return *node, nil
-	}
-	for _, arm := range node.Nodes {
-		res, err := SubNodeByName(&arm, name)
-		if err == nil {
-			return res, err
-		}
-	}
-	return i3ipc.I3Node{}, errors.New("no such Node")
-}
-
-// SubNodesWithoutName gets the subnodes of an I3Node which doesn't match to a specific name
-func SubNodesWithoutName(node *i3ipc.I3Node, name string) (nodes []i3ipc.I3Node, err error) {
-	result := make([]i3ipc.I3Node, len(node.Nodes))
-	for i, item := range node.Nodes {
-		if strings.Compare(item.Name, name) != 0 {
-			result[i] = item
-		}
-	}
-	return result, nil
-}
-
-// FlattenNode gets the ends of the trees
-func FlattenNode(node *i3ipc.I3Node) (nodes []i3ipc.I3Node, err error) {
-	if len(node.Nodes) == 0 {
-		result := make([]i3ipc.I3Node, 1)
-		result[0] = *node
-		return result, nil
-	}
-
-	result := make([]i3ipc.I3Node, 0)
-	for _, item := range node.Nodes {
-		oldnodes := result
-		newnodes, _ := FlattenNode(&item)
-		result = make([]i3ipc.I3Node, len(oldnodes)+len(newnodes))
-		for i, item := range oldnodes {
-			result[i] = item
-		}
-		for i, item := range newnodes {
-			result[i+len(oldnodes)] = item
-		}
-	}
-	return result, nil
 }
 
 // EventLoop - main event loop
 func EventLoop(events chan i3ipc.Event, ipcsocket *i3ipc.IPCSocket, config map[string]string, verbose bool) {
 	for range events {
 		tree, _ := ipcsocket.GetTree()
-		screens, _ := SubNodesWithoutName(&tree, "__i3")
-		for _, screen := range screens {
-			wss, _ := SubNodeByName(&screen, "content")
-			for _, ws := range wss.Nodes {
-				name := ws.Name
-				number := strings.Split(name, " ")[0]
-				windows, _ := FlattenNode(&ws)
-				newname := number
-				windownames := make([]string, len(windows))
-				for i, win := range windows {
-					winname:=strings.ToLower(win.WindowProperties.Class)
-					if(verbose){
-						fmt.Println(winname)
-					}
-					// rename window to config item, if present
-					if val, ok := config[winname]; ok {
-						winname = val
-					} else if len(winname) > 7 {
-						winname = winname[:4] + ".." + winname[len(winname)-3:]
-					}
-					// check if workspace name already contains window title
-					choose := true
-					for _, n := range windownames {
-						if strings.Compare(n, winname) == 0 {
-							choose = false
-						}
-					}
-					if choose {
-						windownames[i] = winname
+		wss := tree.Workspaces()
+		for _, ws := range wss {
+			name := ws.Name
+			number := strings.Split(name, " ")[0]
+			windows := ws.Leaves()
+			newname := number
+			windownames := make([]string, len(windows))
+			for i, win := range windows {
+				winname := strings.ToLower(win.Window_Properties.Class)
+				if verbose {
+					fmt.Println(winname)
+				}
+				// rename window to config item, if present
+				if val, ok := config[winname]; ok {
+					winname = val
+				} else if len(winname) > 7 {
+					winname = winname[:4] + ".." + winname[len(winname)-3:]
+				}
+				// check if workspace name already contains window title
+				choose := true
+				for _, n := range windownames {
+					if strings.Compare(n, winname) == 0 {
+						choose = false
 					}
 				}
-				// rename workspace
-				for _, windowname := range windownames {
-					if(len(windowname) > 0) {
-						newname = fmt.Sprintf("%s %s", newname, windowname)
-					}
+				if choose {
+					windownames[i] = winname
 				}
-				ipcsocket.Command("rename workspace \"" + name + "\" to " + newname)
 			}
+			// rename workspace
+			for _, windowname := range windownames {
+				if len(windowname) > 0 {
+					newname = fmt.Sprintf("%s %s", newname, windowname)
+				}
+			}
+			ipcsocket.Command("rename workspace \"" + name + "\" to " + newname)
 		}
 	}
 }
